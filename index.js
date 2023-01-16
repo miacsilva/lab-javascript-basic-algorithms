be29af781c8b5dc8f498f024ea7ff21d9505d8b2
miacsilva <miacsilvac@hotmail.com>
2023-01-16 19:22:19 +0000

    Commiting Final Results

--- index.js
+++ index.js
@@ -55,3 +55,40 @@ if (hacker1.localeCompare(hacker2) === -1){
     console.log(stringC)
 }
 
+// Bonus Time! 1
+
+let longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet tincidunt urna, at volutpat purus. Suspendisse quis lacinia justo, nec iaculis ante. Cras consectetur purus volutpat dolor consectetur porta. Praesent egestas tortor a ligula volutpat, et scelerisque diam pulvinar. Nam eget lacinia purus, a maximus dolor. Ut maximus nisl at porttitor accumsan. Donec at magna lorem. Aliquam erat volutpat. Aliquam nec mi nisi. Sed laoreet justo id purus auctor mollis. Nulla a sodales augue. Aenean fermentum porta libero, ornare tempus ante dapibus ut. Vestibulum rhoncus fringilla eros, quis venenatis nunc porta in. Proin ultricies vitae massa non pellentesque. Curabitur congue purus vel nibh vestibulum condimentum. In vel quam id purus tempus pellentesque vitae sed nibh. Nulla consequat nulla sit amet cursus consequat. Etiam facilisis euismod purus consequat efficitur. Mauris ornare neque ac dui porttitor dignissim ut id nisi. Vestibulum vitae sapien ut lacus vestibulum fringilla sed in arcu. Aliquam est ligula, scelerisque et elit ac, eleifend consectetur ex. Vestibulum vestibulum tellus libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sollicitudin non tortor id malesuada. Nam ac egestas enim, et malesuada dolor. Morbi volutpat magna non mauris auctor vulputate. Aliquam iaculis elit ut tincidunt mollis."
+
+console.log(longText.length)
+
+let count = 0
+for(let i= 0; i<=longText.length; i++){
+    const twoChart = longText[i] + longText[i+1]
+    if (twoChart === "et"){
+        count++
+    }
+}
+
+console.log(count)
+
+
+
+//Bonus Time! 2
+
+let phraseToCheck = "step on no pets"
+
+
+let reversedPhrase = ""
+for( let i = phraseToCheck.length-1; i>=0; i--){
+    
+    reversedPhrase += phraseToCheck[i]
+}
+
+if(phraseToCheck === reversedPhrase){
+    console.log(`${phraseToCheck} is a Palindrome!`)
+}else {
+    console.log(`${phraseToCheck} is not a Palindrome!`)  
+    }
+
+
+    
